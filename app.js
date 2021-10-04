420457bdc366069fa0b72a777ac7ac0562aabf0e
Sarah Baghdadi <sera.baghdadi@gmail.com>
2021-10-03 20:33:55 -0400

    Implement id url param and error handling

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 
 app.set('view engine', 'pug');
 app.use('/static', express.static('public'));
-const data = app.use(express.static('data.json'));
+const data = require('./data.json');
 
 app.get('/', (req, res) => {
     res.locals = data.projects;
@@ -14,9 +14,37 @@ app.get('/about', (req, res) => {
     res.render('about');
 });
 
-// app.get(`/projects/:id`, (req, res) => {
-//     res.render(`/projects/${id}`);
-// });
+app.get(`/projects/:id`, (req, res) => {
+    const id = req.params.id;
+    const project = data.projects[id];
+    res.render('project', project);
+});
+
+// catch 404 and forward to error handler
+app.use((req, res, next) => {
+    const err = new Error;
+    err.status = 404;
+    err.message = 'Page Not Found';
+    next(err);
+  });
+  
+  // error handlers
+  
+  app.use((err, req, res, next) => {
+    if (!err.status) { 
+        err.status = 500;
+    };
+    if (!err.message) {
+        err.message = 'Internal Server Error'; 
+    };
+    console.log(`Error status is ${err.status} and message is ${err.message}.`);
+    res.render('error', {
+      message: err.message,
+      status: err.status,
+      error: {}
+    });
+  });
+  
 
 app.listen(3000, () => {
     console.log('App is running on 3000!');
